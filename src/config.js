0a787f5bba8dd47135b1d7c7c02242b1ee85d4b5
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-07-29 20:58:25 +0300

    bug

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'process.env.REACT_APP_API_URL'
-export const AUTH_URL = `${API_BASE_URL}/Auth`
-export const RECORDS_URL = `${API_BASE_URL}/records`
+export const API_BASE_URL = process.env.REACT_APP_API_URL;
+export const AUTH_URL = `${API_BASE_URL}/Auth`;
+export const RECORDS_URL = `${API_BASE_URL}/records`;
